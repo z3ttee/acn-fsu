e27aff2e3d159bd3321f03be9e56e80c11bd1dbf
Cedric Zitzmann <cedric.zitzmann@gmail.com>
2021-03-22 15:26:42 +0100

    added fitness tracker example

---
+++ spring/fitnesstracker/src/test/java/com/pluralsight/fitnesstracker/FitnesstrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pluralsight.fitnesstracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitnesstrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
