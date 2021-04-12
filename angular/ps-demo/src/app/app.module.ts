aeadd256125a1781956f7cf8fadb0e7425fe3fcc
Cedric Zitzmann <cedric.zitzmann@gmail.com>
2021-04-12 11:13:11 +0200

    added form with bootstrap styling

--- angular/ps-demo/src/app/app.module.ts
+++ angular/ps-demo/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from "@angular/forms"
 
 import { AppComponent } from './app.component';
+import { UserSettingsFormComponent } from './user-settings-form/user-settings-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserSettingsFormComponent
   ],
   imports: [
     BrowserModule,
