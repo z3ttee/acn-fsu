38badf43586fd77af8a8c7875da9710c1c34defd
Cedric Zitzmann <cedric.zitzmann@gmail.com>
2021-04-13 13:28:10 +0200

    init

--- angular/angular-material/src/app/app.component.spec.ts
+++ angular/angular-material/src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'angularmaterial'`, () => {
+  it(`should have as title 'angular-material'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('angularmaterial');
+    expect(app.title).toEqual('angular-material');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('angularmaterial app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('angular-material app is running!');
   });
 });
--- angular/angular-material/src/app/app.module.ts
+++ angular/angular-material/src/app/app.module.ts
@@ -1,21 +1,14 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
-import { MaterialModule } from './shared/material.module';
-import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule,
-    BrowserAnimationsModule,
-    MaterialModule,
-    FormsModule
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
