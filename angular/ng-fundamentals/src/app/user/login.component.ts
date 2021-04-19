87b65c179039d0f46446b4817478366c65f3a646
Cedric Zitzmann <cedric.zitzmann@gmail.com>
2021-04-19 13:44:10 +0200

    template based form

--- angular/ng-fundamentals/src/app/user/login.component.ts
+++ angular/ng-fundamentals/src/app/user/login.component.ts
@@ -1,19 +1,32 @@
 import { Component } from "@angular/core";
+import { Router } from "@angular/router";
 import { AuthService } from "./auth.service";
 
 
 @Component({
-    templateUrl: "./login.component.html"
+    templateUrl: "./login.component.html",
+    styles: [`
+        em {
+            float: right;
+            padding-left: 10px;
+            color: #e05c65;
+        }
+    `]
 })
 export class LoginComponent {
 
     public username: string = "";
     public password: string = "";
 
-    constructor(private authService: AuthService){}
+    constructor(private authService: AuthService, private router: Router){}
 
     public login(formValues: any): void {
         this.authService.loginUser(formValues.username, formValues.password)
+        this.router.navigate(["/"])
+    }
+
+    public cancel(): void {
+        this.router.navigate(["/"])
     }
 
 }
