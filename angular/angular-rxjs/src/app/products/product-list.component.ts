0369d0c7edb9c50d5d951e060ef31256f16a8b1c
Cedric Zitzmann <cedric.zitzmann@gmail.com>
2021-04-20 15:42:20 +0200

    added filtering

--- angular/angular-rxjs/src/app/products/product-list.component.ts
+++ angular/angular-rxjs/src/app/products/product-list.component.ts
@@ -1,7 +1,8 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 import { EMPTY } from 'rxjs';
-import { catchError } from 'rxjs/operators';
+import { catchError, map } from 'rxjs/operators';
+import { ProductCategoryService } from '../product-categories/product-category.service';
 
 import { ProductService } from './product.service';
 
@@ -16,21 +17,34 @@ import { ProductService } from './product.service';
 export class ProductListComponent {
   pageTitle = 'Product List';
   errorMessage = '';
-  categories;
+  selectedCategoryId = 1;
 
   products$ = this.productService.productsWithCategories$.pipe(
     catchError((err) => {
       this.errorMessage = err
       return EMPTY; // or use: of([])
-    }));
+    })
+  );
 
-  constructor(private productService: ProductService) { }
+  categories$ = this.productCategoryService.productCategories$.pipe(
+    catchError(err => {
+      this.errorMessage = err;
+      return EMPTY;
+    })
+  );
+
+  productsSimpleFilter$ = this.productService.productsWithCategories$.pipe(
+    map(products => products.filter(product => this.selectedCategoryId ? product.categoryId === this.selectedCategoryId : true))
+  );
+
+  constructor(private productService: ProductService, 
+              private productCategoryService: ProductCategoryService) { }
 
   onAdd(): void {
     console.log('Not yet implemented');
   }
 
   onSelected(categoryId: string): void {
-    console.log('Not yet implemented');
+    this.selectedCategoryId = +categoryId;
   }
 }
