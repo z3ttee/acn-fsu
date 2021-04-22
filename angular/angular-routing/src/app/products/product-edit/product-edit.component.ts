0e69d30c4c67b2e840b1b4c5af9e2c9a6d3c6f37
Cedric Zitzmann <cedric.zitzmann@gmail.com>
2021-04-22 14:55:41 +0200

    validation across routes

--- angular/angular-routing/src/app/products/product-edit/product-edit.component.ts
+++ angular/angular-routing/src/app/products/product-edit/product-edit.component.ts
@@ -16,6 +16,9 @@ export class ProductEditComponent implements OnInit {
 
   product: Product;
 
+  // Key/Value Pair
+  private dataIsValid: { [key: string ]: boolean } = {}
+
   constructor(private productService: ProductService,
               private messageService: MessageService,
               private router: Router,
@@ -61,7 +64,7 @@ export class ProductEditComponent implements OnInit {
   }
 
   saveProduct(): void {
-    if (true === true) {
+    if (this.isValid()) {
       if (this.product.id === 0) {
         this.productService.createProduct(this.product).subscribe({
           next: () => this.onSaveComplete(`The new ${this.product.productName} was saved`),
@@ -94,5 +97,32 @@ export class ProductEditComponent implements OnInit {
       this.errorMessage = resolvedData.error
       this.onProductRetrieved(resolvedData.product)
     })
+  }
+
+  public validate(): void {
+    // Reset validation
+    this.dataIsValid = {};
+
+    // "info" tab
+    if(this.product.productName && this.product.productName.length >= 3 && this.product.productCode) {
+      this.dataIsValid["info"] = true;
+    } else {
+      this.dataIsValid["info"] = false;
+    }
+
+    // "tags" tab
+    if(this.product.category && this.product.category.length >= 3) {
+      this.dataIsValid["tags"] = true;
+    } else {
+      this.dataIsValid["tags"] = false;
+    }
+  }
+
+  public isValid(path?: string): boolean {
+    this.validate();
+
+    if(path) return this.dataIsValid[path];
+
+    return this.dataIsValid && Object.keys(this.dataIsValid).every(d => this.dataIsValid[d] === true)
   }
 }
